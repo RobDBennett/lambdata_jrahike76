befa44941a5736515aedb8519854679a7a1df895
Jay Adamo <63022977+JraHike76@users.noreply.github.com>
2020-07-07 14:35:49 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
 """
 lambdata - a collection of Data Science helper functions
 """
 import pandas as pd
 import numpy as np
 from . import example_module 
+
+# Writing functions to run
+# Defining TEST and Y
 TEST = pd.DataFrame(np.ones(10))
 Y = example_module.increment(example_module.x)
